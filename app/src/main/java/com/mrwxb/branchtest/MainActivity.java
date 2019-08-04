8806450779cc05606e4d9ca7a0c7aac30d38ea15
GitHubwbb123 <GitHubwbb123@users.github.com>
2019-08-05 02:29:52 +0800

    Merge branch 'version1.0'
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/java/com/mrwxb/branchtest/MainActivity.java

--- app/src/main/java/com/mrwxb/branchtest/MainActivity.java
+++ app/src/main/java/com/mrwxb/branchtest/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this,"我是version1.0分之", Toast.LENGTH_SHORT).show();
         Toast.makeText(MainActivity.this,"合并后悔怎样", Toast.LENGTH_SHORT).show();
         Toast.makeText(MainActivity.this,"我是主分之",Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this,"到底咋呢高",Toast.LENGTH_SHORT).show();
     }
 }
